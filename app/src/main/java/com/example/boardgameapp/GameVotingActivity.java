785410026896ce8717fc528d0f5bd61c52d2d2ba
LeonRohde <leonrohde95@gmail.com>
2023-08-18 17:06:02 +0200

    SQLite Anbindung mit Crash. Ohne Fehler in GameVotingActivity

--- app/src/main/java/com/example/boardgameapp/GameVotingActivity.java
+++ app/src/main/java/com/example/boardgameapp/GameVotingActivity.java
@@ -1,11 +1,11 @@
 package com.example.boardgameapp;
-import android.util.Log;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class GameVotingActivity extends AppCompatActivity {
 
@@ -61,6 +60,7 @@ public class GameVotingActivity extends AppCompatActivity {
     }
 
 
+    @SuppressLint("Range")
     private int getVotes(String game) {
         SQLiteDatabase db = databaseHelper.getReadableDatabase();
         String[] projection = {VotingDatabaseHelper.COLUMN_VOTES};
