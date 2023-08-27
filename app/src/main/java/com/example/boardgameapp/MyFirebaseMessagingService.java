abb50a629f5ea2ba48ce29875c02532bca6380e0
AlexanderPuhle <alexander.puhle1@iubh-fernstudium.de>
2023-08-27 08:18:42 +0200

    Notification von Firebase empfangen

--- app/src/main/java/com/example/boardgameapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/boardgameapp/MyFirebaseMessagingService.java
@@ -1,6 +1,31 @@
 package com.example.boardgameapp;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        getFirebaseMessage(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    @SuppressLint("MissingPermission")
+    public void getFirebaseMessage(String title, String msg)
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"myFirebaseChannel")
+                .setSmallIcon(R.drawable.baseline_games_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(101,builder.build());
+    }
+
 }
