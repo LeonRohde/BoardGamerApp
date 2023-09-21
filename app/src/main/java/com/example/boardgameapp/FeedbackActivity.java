d4dab1f5aee595937fe1d9d231f5c65296cb284a
LeonRohde <leonrohde95@gmail.com>
2023-09-21 11:52:18 +0200

    FeedbackActivity funktioniert wieder mit Datenbankanbindung nach Datenverlust.

--- app/src/main/java/com/example/boardgameapp/FeedbackActivity.java
+++ app/src/main/java/com/example/boardgameapp/FeedbackActivity.java
@@ -1,22 +1,38 @@
 package com.example.boardgameapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.Call;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FeedbackActivity extends AppCompatActivity {
 
     private RatingBar hostRatingBar;
     private RatingBar foodRatingBar;
-    private RatingBar overallRatingBar;
+    private RatingBar eveningRatingBar;
+    private Spinner spielerSpinner;
     private Button submitButton;
-    private AppDatabase appDatabase;
+
+    private List<String> spielerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,52 +41,133 @@ public class FeedbackActivity extends AppCompatActivity {
 
         hostRatingBar = findViewById(R.id.hostRatingBar);
         foodRatingBar = findViewById(R.id.foodRatingBar);
-        overallRatingBar = findViewById(R.id.overallRatingBar);
+        eveningRatingBar = findViewById(R.id.eveningRatingBar);
+        spielerSpinner = findViewById(R.id.spielerSpinner);
         submitButton = findViewById(R.id.submitButton);
 
-        appDatabase = AppDatabase.getInstance(this);
+        spielerList = new ArrayList<>();
 
-        submitButton.setOnClickListener(v -> saveFeedback());
+        getSpielerFromDatabase();
+
+        ArrayAdapter<String> spielerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spielerList);
+        spielerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spielerSpinner.setAdapter(spielerAdapter);
+
+        submitButton.setOnClickListener(v -> {
+            saveFeedback();
+            displayAverageRating();
+        });
     }
+
+    private void getSpielerFromDatabase() {
+        OkHttpClient client = new OkHttpClient();
+        String serverURL = "https://qu-iu-zz.beyer-its.de/select_feedback.php";
+
+        Request request = new Request.Builder()
+                .url(serverURL)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() -> {
+                    Toast.makeText(FeedbackActivity.this, "Fehler beim Abrufen der Spieler", Toast.LENGTH_SHORT).show();
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+
+                    try {
+                        JSONArray jsonArray = new JSONArray(responseData);
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            String spieler = jsonArray.getString(i);
+                            spielerList.add(spieler);
+                        }
+
+                        runOnUiThread(() -> {
+                            ArrayAdapter<String> spielerAdapter = new ArrayAdapter<>(FeedbackActivity.this, android.R.layout.simple_spinner_item, spielerList);
+                            spielerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            spielerSpinner.setAdapter(spielerAdapter);
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    runOnUiThread(() -> {
+                        Toast.makeText(FeedbackActivity.this, "Fehler beim Abrufen der Spieler", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            }
+        });
+    }
+
     private void saveFeedback() {
         float hostRating = hostRatingBar.getRating();
         float foodRating = foodRatingBar.getRating();
-        float overallRating = overallRatingBar.getRating();
+        float eveningRating = eveningRatingBar.getRating();
 
-        Feedback feedback = new Feedback();
-        feedback.setHostRating(hostRating);
-        feedback.setFoodRating(foodRating);
-        feedback.setOverallRating(overallRating);
+        String selectedSpieler = spielerSpinner.getSelectedItem().toString();
 
-        AppExecutor.getInstance().diskIO().execute(() -> {
-            appDatabase.feedbackDao().insertFeedback(feedback);
-            float averageRating = calculateAverageRating();
-            runOnUiThread(() -> {
-                showAverageRating(averageRating);
-                Toast.makeText(this, "Feedback gespeichert", Toast.LENGTH_SHORT).show();
-            });
+        sendFeedbackToServer(selectedSpieler, hostRating, foodRating, eveningRating);
+    }
+
+    private void sendFeedbackToServer(String selectedSpieler, float hostRating, float foodRating, float eveningRating) {
+        OkHttpClient client = new OkHttpClient();
+        String serverURL = "https://qu-iu-zz.beyer-its.de/update_feedback.php";
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("Spieler", selectedSpieler)
+                .add("Gastgeber", String.valueOf(hostRating))
+                .add("Essen", String.valueOf(foodRating))
+                .add("Abend", String.valueOf(eveningRating))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(serverURL)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() -> {
+                    Toast.makeText(FeedbackActivity.this, "Fehler beim Senden des Feedbacks", Toast.LENGTH_SHORT).show();
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    runOnUiThread(() -> {
+                        Toast.makeText(FeedbackActivity.this, "Feedback erfolgreich gespeichert", Toast.LENGTH_SHORT).show();
+                    });
+                } else {
+                    runOnUiThread(() -> {
+                        Toast.makeText(FeedbackActivity.this, "Fehler beim Speichern des Feedbacks", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            }
         });
     }
-    private void showAverageRating(float averageRating) {
-        TextView averageRatingTextView = findViewById(R.id.averageRatingTextView);
+
+    private void displayAverageRating() {
+        float hostRating = hostRatingBar.getRating();
+        float foodRating = foodRatingBar.getRating();
+        float eveningRating = eveningRatingBar.getRating();
+
+        float averageRating = (hostRating + foodRating + eveningRating) / 3;
+
+        // Begrenze auf eine Dezimalstelle
         String formattedRating = String.format("%.1f", averageRating);
+
+        TextView averageRatingTextView = findViewById(R.id.averageRatingTextView);
         averageRatingTextView.setText("Durchschnittliche Bewertung: " + formattedRating);
-    }
-
-
-    private float calculateAverageRating() {
-        List<Feedback> allFeedback = appDatabase.feedbackDao().getAllFeedback();
-
-        if (allFeedback.isEmpty()) {
-            return 0.0f; // Keine Bewertungen vorhanden, Durchschnitt = 0
-        }
-
-        float totalRating = 0.0f;
-        for (Feedback feedback : allFeedback) {
-            totalRating += feedback.getHostRating();
-        }
-
-        return totalRating / allFeedback.size();
     }
 
 }
